537cac7acef580b218cd424bea2873aa074d8e51
Jonathan Hirschberg <jonathan.hirschberg@gmail.com>
2013-01-26 00:44:45 -0800

    defined the AllergyType enumeration, declared variables for Character

--- Unity/Assets/Scripts/Character.cs
+++ Unity/Assets/Scripts/Character.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Character : MonoBehaviour
 {
-	int HitPoints;
+	public int HitPoints;
+	public Dictionary<int, int> Allergies = new Dictionary<int, int>();
+	public int[] Abilities;
+	
+	void Start()
+	{
+	}
+	
+	void Update()
+	{
+	}
 }
