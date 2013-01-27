70ee5c770d216fec87d564a08cab19b7abd33f19
Francisco Olvera <LostJourneyer@gmail.com>
2013-01-27 11:59:20 -0800

    Build added

--- Allergy/Assets/Scripts/Menu.cs
+++ Allergy/Assets/Scripts/Menu.cs
@@ -3,9 +3,21 @@ using System.Collections;
 
 public class Menu : MonoBehaviour
 {
+	public Texture[] TextureList;
+	
+	int size, current = 0;
+	void Start()
+	{
+		size = TextureList.Length;
+	}
+	
 	void OnGUI()
 	{
-		if(GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 100, 50), "Start Game"))
-			Application.LoadLevel("Level1");
+		if(GUI.Button(new Rect(0, 0, Screen.width, Screen.height), TextureList[current]))
+		{
+			if(current == size-1)
+				Application.LoadLevel("Level1");
+			current++;
+		}
 	}
 }
