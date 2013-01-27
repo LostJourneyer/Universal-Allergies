d1979a72586d6ffe7517478e3b6e2af0b5ecf9b3
Francisco Olvera <LostJourneyer@gmail.com>
2013-01-26 16:28:45 -0800

    Enemies can move between waypoints

--- Allergy/Assets/Scripts/Enemy.cs
+++ Allergy/Assets/Scripts/Enemy.cs
@@ -4,4 +4,21 @@ using System.Collections;
 public class Enemy : MonoBehaviour
 {
 	public AllergyType EnemyType;
+	public NavMeshAgent agent;
+	public Vector3[] waypoints;
+	int current=0;
+	void Start()
+	{
+		agent.destination = waypoints[0];
+	}
+	void Update()
+	{
+		if(Vector3.Distance(transform.position, waypoints[current]) < 10)
+		{
+			current++;
+			if(current >= waypoints.Length)
+				current = 0;
+			agent.destination = waypoints[current];
+		}
+	}
 }
